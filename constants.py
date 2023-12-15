b73e71adf39e687dc9391458ea717f965c895cbe
SullivanC19 <colinsullivan203@gmail.com>
2023-12-14 19:44:18 -0600

    tweaked constants

--- constants.py
+++ constants.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-SEED = 35
+SEED = 42
 RNG = np.random.Generator(np.random.PCG64(SEED))
 
 # only support centered uniform dists [-1, 1]
 F_EPS = lambda n_arms: RNG.uniform(-1, 1, size=n_arms)
 
-C = 20
+C = 50
 ALPHA = 0.01
-T = 10000
+T = 20000
 
 CDICT = {
   'ucb_at': 'orange',
